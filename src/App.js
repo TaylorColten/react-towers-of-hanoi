d6e00df5728cc23c546b4c3111706621f8337306
unknown <taylorwcolten@gmail.com>
2019-01-21 17:33:29 -0600

    start

--- src/App.js
+++ src/App.js
@@ -2,27 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+class TowersOfHanoi extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grabbedBlock: 0,
+      a: [100,75,50,25],
+      b: [],
+      c: [],
+    }
+    this.game = this.game.bind(this);
   }
-}
 
-export default App;
+  
